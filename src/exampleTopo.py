2fe21912a1fc0f56950324282735e061b777b43e
forward0606 <chen910606@gmail.com>
2024-12-03 16:15:30 +0800

    update the topo

--- src/exampleTopo.py
+++ src/exampleTopo.py
@@ -7,26 +7,16 @@ class Topology(Topo):
         # Initialize topology
         Topo.__init__(self)
 
-        os.system("figlet Lab3 Topo")
-	
 	# Add hosts into topology
         h1 = self.addHost('h1')
         h2 = self.addHost('h2')
 
         # Add switches into topology
         s1 = self.addSwitch('s1')
-        s2 = self.addSwitch('s2')
-        s3 = self.addSwitch('s3')
-        s4 = self.addSwitch('s4')
 
         # Add links into topology
-	# [TODO] Add contraints(bw, delay, loss) given in topo.png
         self.addLink(s1, h1, port1=1, port2=1)
-        self.addLink(s1, s2, port1=2, port2=1)
-        self.addLink(s1, s4, port1=3, port2=3)
-        self.addLink(s4, h2, port1=1, port2=1)
-        self.addLink(s4, s3, port1=2, port2=1)
-        self.addLink(s3, s2, port1=2, port2=2)
+        self.addLink(s1, h2, port1=2, port2=1)
 	      
 
 topos = {
